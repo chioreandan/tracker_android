0ba8c14118425fde1d0c3de64a474664a73bebe5
Dan Chiorean <chiorean.dan12@gmail.com>
2018-12-02 11:15:34 +0200

    Add rounded shap button and make location work via gps and network

--- app/src/main/java/com/example/dan/tracker_android/Activities/MainActivity.kt
+++ app/src/main/java/com/example/dan/tracker_android/Activities/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.example.dan.tracker_android.Activities
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
 import com.example.dan.tracker_android.R
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
@@ -11,25 +21,180 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.LocationSettingsRequest
 import kotlinx.android.synthetic.main.activity_main.*
 
+private const val PERMISSION_REQUEST = 10
+
 class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    lateinit var locationManager: LocationManager
+    private var hasGps = false
+    private var hasNetwork = false
+    private var locationGps: Location? = null
+    private var locationNetwork: Location? = null
+
+    private var permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        disableView()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkPermission(permissions)) {
+                enableView()
+            } else {
+                requestPermissions(permissions, PERMISSION_REQUEST)
+            }
+        } else {
+            enableView()
+        }
+
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        button2.setOnClickListener {
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                if(location!=null){
-                    this.textView4.text= location.longitude.toString()
-                    this.textView3.text = location.latitude.toString()
-                } else{
-                    this.textView4.text = "Could not get location"
+//        button2.setOnClickListener {
+//            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+//                if (location != null) {
+//                    this.textView4.text = location.longitude.toString()
+//                    this.textView3.text = location.latitude.toString()
+//                } else {
+//                    this.textView4.text = "Could not get location"
+//                }
+//            }
+//        }
+    }
+
+    private fun disableView() {
+        button2.isEnabled = false
+        button2.alpha = 0.5F
+    }
+
+    private fun enableView() {
+        button2.isEnabled = true
+        button2.alpha = 1F
+        button2.setOnClickListener { getLocation()}
+        Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show()
+    }
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        if (hasGps || hasNetwork) {
+
+            if (hasGps) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0F, object :
+                    LocationListener {
+                    override fun onLocationChanged(location: Location?) {
+                        if (location != null) {
+                            locationGps = location
+                            textView4.append("\n" + locationNetwork!!.latitude )
+                            textView3.append("\n" + locationNetwork!!.longitude )
+//                            textView4.text = locationGps!!.latitude.toString()
+//                            textView3.text = locationGps!!.longitude.toString()
+                        }
+                    }
+
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+                    }
+
+                    override fun onProviderEnabled(provider: String?) {
+
+                    }
+
+                    override fun onProviderDisabled(provider: String?) {
+
+                    }
+
+                })
+
+                val localGpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                if (localGpsLocation != null)
+                    locationGps = localGpsLocation
+            }
+            if (hasNetwork) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0F, object :
+                    LocationListener {
+                    override fun onLocationChanged(location: Location?) {
+                        if (location != null) {
+                            locationNetwork = location
+                            textView4.append("\n" + locationNetwork!!.latitude )
+                            textView3.append("\n" + locationNetwork!!.longitude )
+
+//                            textView4.text =  locationNetwork!!.latitude.toString()
+//                            textView3.text = locationNetwork!!.longitude.toString()
+                        }
+                    }
+
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+                    }
+
+                    override fun onProviderEnabled(provider: String?) {
+
+                    }
+
+                    override fun onProviderDisabled(provider: String?) {
+
+                    }
+
+                })
+
+                val localNetworkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                if (localNetworkLocation != null)
+                    locationNetwork = localNetworkLocation
+            }
+
+            if(locationGps!= null && locationNetwork!= null){
+                if(locationGps!!.accuracy > locationNetwork!!.accuracy){
+                    tv_result.append("\nNetwork ")
+                    tv_result.append("\nLatitude : " + locationNetwork!!.latitude)
+                    tv_result.append("\nLongitude : " + locationNetwork!!.longitude)
+                    Log.d("CodeAndroidLocation", " Network Latitude : " + locationNetwork!!.latitude)
+                    Log.d("CodeAndroidLocation", " Network Longitude : " + locationNetwork!!.longitude)
+                }else{
+                    tv_result.append("\nGPS ")
+                    tv_result.append("\nLatitude : " + locationGps!!.latitude)
+                    tv_result.append("\nLongitude : " + locationGps!!.longitude)
+                    Log.d("CodeAndroidLocation", " GPS Latitude : " + locationGps!!.latitude)
+                    Log.d("CodeAndroidLocation", " GPS Longitude : " + locationGps!!.longitude)
                 }
             }
+
+        } else {
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        }
+    }
+
+
+    private fun checkPermission(permissionArray: Array<String>): Boolean {
+        var allSuccess = true
+        for (i in permissionArray.indices) {
+            if (checkCallingOrSelfPermission(permissionArray[i]) == PackageManager.PERMISSION_DENIED)
+                allSuccess = false
+        }
+        return allSuccess
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST) {
+            var allSuccess = true
+            for (i in permissions.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    allSuccess = false
+                    val requestAgain = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && shouldShowRequestPermissionRationale(permissions[i])
+                    if (requestAgain) {
+                        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Go to settings and enable the permission", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            if (allSuccess)
+                enableView()
+
         }
     }
 }
